b40b9bde00ba6aa4b7cff41e91efbd904ba7bc92
Vito <purview-joust.0x@icloud.com>
2024-02-10 18:49:13 +0000

    resources

--- Touchdown/Utility/Constant.swift
+++ Touchdown/Utility/Constant.swift
@@ -7,3 +7,15 @@
 
 import SwiftUI
 
+// DATA
+// COLOR
+let colorBackground = Color("ColorBackground")
+let colorGray = Color(UIColor.systemGray4)
+
+// LAYOUT
+// UX
+// API
+// IMAGE
+// FONT
+// STRING
+// MISC

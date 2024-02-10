0bed48c55efed7117d57ae527ffb6b5b55cf8040
Vito <purview-joust.0x@icloud.com>
2024-02-10 18:36:51 +0000

    Initial Commit

---
+++ Touchdown/TouchdownApp.swift
@@ -0,0 +1,17 @@
+//
+//  TouchdownApp.swift
+//  Touchdown
+//
+//  Created by Vito Borghi on 10/02/2024.
+//
+
+import SwiftUI
+
+@main
+struct TouchdownApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

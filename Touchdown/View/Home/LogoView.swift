84cf25296f640b76656f43ce89bf8d2059ded3c1
Vito <purview-joust.0x@icloud.com>
2024-02-11 14:28:03 +0000

    NavigationBarView

---
+++ Touchdown/View/Home/LogoView.swift
@@ -0,0 +1,18 @@
+//
+//  LogoView.swift
+//  Touchdown
+//
+//  Created by Vito Borghi on 11/02/2024.
+//
+
+import SwiftUI
+
+struct LogoView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    LogoView()
+}
---
+++ Touchdown/View/Home/NavigationBarView.swift
@@ -0,0 +1,46 @@
+//
+//  NavigationBarView.swift
+//  Touchdown
+//
+//  Created by Vito Borghi on 11/02/2024.
+//
+
+import SwiftUI
+
+struct NavigationBarView: View {
+    // MARK: - PROPERTIES
+    
+    // MARK: - BODY
+    var body: some View {
+       HStack {
+           Button{
+               //action
+           } label: {
+               Image(systemName: "magnifyingglass")
+                   .font(.title)
+                   .foregroundStyle(.black)
+           }//: Button
+           
+           Spacer()
+           
+           Button{
+               //action
+           } label: {
+               ZStack {
+                   Image(systemName: "cart")
+                       .font(.title)
+                       .foregroundStyle(.black)
+                   
+                   Circle()
+                       .fill(.red)
+                       .frame(width: 14, height: 14, alignment: .center)
+                       .offset(x: 13, y: -10)
+               }
+           }//: BUTTON
+        }//: HSTACK
+    }
+}
+
+#Preview {
+    NavigationBarView()
+}

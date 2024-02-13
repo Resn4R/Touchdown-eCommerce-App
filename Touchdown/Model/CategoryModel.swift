e1bc14ed7f962c40aa2b03f4ed6582a26620a03a
Vito <purview-joust.0x@icloud.com>
2024-02-13 18:56:46 +0000

    categoryModel

---
+++ Touchdown/Model/CategoryModel.swift
@@ -0,0 +1,14 @@
+//
+//  CategoryModel.swift
+//  Touchdown
+//
+//  Created by Vito Borghi on 13/02/2024.
+//
+
+import Foundation
+
+struct Category: Identifiable, Codable {
+    let id: Int
+    let name: String
+    let image: String
+}

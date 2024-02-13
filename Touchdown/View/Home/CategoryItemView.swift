7cc549013ef044a604f12439365beac626495c39
Vito <purview-joust.0x@icloud.com>
2024-02-13 19:09:35 +0000

    categoryItemView

---
+++ Touchdown/View/Home/CategoryItemView.swift
@@ -0,0 +1,47 @@
+//
+//  CategroyItemView.swift
+//  Touchdown
+//
+//  Created by Vito Borghi on 13/02/2024.
+//
+
+import SwiftUI
+
+struct CategoryItemView: View {
+    // MARK: - property
+    
+    let category: Category
+    
+    // MARK: - body
+    var body: some View {
+            Button {
+                
+            } label: {
+                HStack(alignment: .center, spacing: 6) {
+                    Image(category.image)
+                        .renderingMode(.template)
+                        .resizable()
+                        .scaledToFit()
+                        .frame(width: 30, height: 30, alignment: .center)
+                        .foregroundStyle(.gray)
+                    
+                    Text(category.name.uppercased())
+                        .fontWeight(.light)
+                        .foregroundStyle(.gray)
+                    
+                    Spacer()
+                }//: HSTACK
+                .padding()
+                .background {
+                        RoundedRectangle(cornerRadius: 12)
+                            .stroke(.gray, lineWidth: 1)
+                }
+            }//: BUTTON
+    }
+}
+
+#Preview {
+    CategoryItemView(category: categories[0])
+        .padding()
+        .background(colorBackground)
+}

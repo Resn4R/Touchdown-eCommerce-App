7d758c80afaccb1e1602504cd9104a34d402694d
Vito <purview-joust.0x@icloud.com>
2024-02-15 21:08:14 +0000

    brandmodel

---
+++ Touchdown/Model/BrandModel.swift
@@ -0,0 +1,13 @@
+//
+//  BrandModel.swift
+//  Touchdown
+//
+//  Created by Vito Borghi on 15/02/2024.
+//
+
+import Foundation
+
+struct Brand: Codable, Identifiable {
+    let id: Int
+    let image: String
+}
---
+++ Touchdown/View/Home/BrandItemView.swift
@@ -0,0 +1,21 @@
+//
+//  BrandItemView.swift
+//  Touchdown
+//
+//  Created by Vito Borghi on 15/02/2024.
+//
+
+import SwiftUI
+
+struct BrandItemView: View {
+    // MARK: - PROPERTIES
+    @State var brand: Brand
+    // MARK: - BODY
+    var body: some View {
+        
+    }
+}
+
+#Preview {
+    BrandItemView()
+}

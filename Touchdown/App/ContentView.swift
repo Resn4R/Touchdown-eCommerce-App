098a8b1259fa0062bdfe8c742614f94229e75a1f
Vito <purview-joust.0x@icloud.com>
2024-02-16 18:00:25 +0000

    message

--- Touchdown/App/ContentView.swift
+++ Touchdown/App/ContentView.swift
@@ -43,7 +43,6 @@ struct ContentView: View {
                         
                         BrandGridView()
                         
-    
                         FooterView()
                             .padding(.horizontal)
                     }//: VStack

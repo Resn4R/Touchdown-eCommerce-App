7ec9a141c6c3f0371cf6a7f75f90b431040deb78
Vito <purview-joust.0x@icloud.com>
2024-02-17 23:08:29 +0000

    ratings and size view

---
+++ Touchdown/View/Home/RatingsSizesDetailView.swift
@@ -0,0 +1,75 @@
+//
+//  RatingsSizesDetailView.swift
+//  Touchdown
+//
+//  Created by Vito Borghi on 17/02/2024.
+//
+
+import SwiftUI
+
+struct RatingsSizesDetailView: View {
+    let sizes: [String] = ["XS", "S", "M", "L", "XL"]
+    
+    var body: some View {
+        HStack(alignment: .top, spacing: 3) {
+            // RATINGS
+            VStack(alignment: .leading, spacing: 3) {
+                Text("Ratings")
+                    .font(.footnote)
+                    .fontWeight(.semibold)
+                    .foregroundStyle(.gray)
+                
+                HStack(alignment: .center, spacing: 3) {
+                    ForEach(1...5, id: \.self) { item in
+                        Button {
+                            //add rating
+                        } label: {
+                            Image(systemName: "star.fill")
+                                .frame(width: 28, height: 28, alignment: .center)
+                                .background(colorGray.clipShape(RoundedRectangle(cornerRadius: 5)))
+                                .foregroundStyle(.white)
+                        }
+                    }
+                }
+            }
+            
+            
+            Spacer()
+            
+            //SIZE
+            VStack(alignment: .trailing, spacing: 3) {
+                Text("Sizes")
+                    .font(.footnote)
+                    .fontWeight(.semibold)
+                    .foregroundStyle(colorGray)
+                
+                HStack(alignment: .center, spacing: 5) {
+                    ForEach(sizes, id: \.self) { size in
+                        Button {
+                            //choose size
+                        } label: {
+                            Text(size)
+                                .font(.footnote)
+                                .fontWeight(.heavy)
+                                .foregroundStyle(colorGray)
+                                .frame(width: 28, height: 28, alignment: .center)
+                                .background{
+                                    ZStack {
+                                        Color.white.clipShape( RoundedRectangle(cornerRadius: 5) )
+                                        RoundedRectangle(cornerRadius: 5)
+                                            .stroke(colorGray, lineWidth: 2)
+                                    }
+                                }
+                        }
+                    }
+                }
+            }//: VStack
+        }//: HSTACK
+    }
+}
+
+#Preview {
+    RatingsSizesDetailView()
+        .previewLayout(.sizeThatFits)
+        .padding()
+}

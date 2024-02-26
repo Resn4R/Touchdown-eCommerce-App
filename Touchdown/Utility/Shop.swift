f10e7d361dee4b9c0f14956542d7d9c33b4dd3dd
Vito <purview-joust.0x@icloud.com>
2024-02-26 10:39:04 +0000

    shop class

---
+++ Touchdown/Utility/Shop.swift
@@ -0,0 +1,13 @@
+//
+//  Shop.swift
+//  Touchdown
+//
+//  Created by Vito Borghi on 26/02/2024.
+//
+
+import Foundation
+
+class Shop {
+    var showingProducts = false
+    var selectedProduct: Product?
+}
